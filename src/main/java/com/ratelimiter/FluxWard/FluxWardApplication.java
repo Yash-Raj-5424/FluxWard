8a53ce3b3930efa89484b67d37d915d76b802836
Yash-Raj-5424 <myash3499@gmail.com>
2026-04-09 18:57:12 +0530

    Initial setup

---
+++ src/main/java/com/ratelimiter/FluxWard/FluxWardApplication.java
@@ -0,0 +1,13 @@
+package com.ratelimiter.FluxWard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxWardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluxWardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ratelimiter/FluxWard/FluxWardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratelimiter.FluxWard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxWardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
