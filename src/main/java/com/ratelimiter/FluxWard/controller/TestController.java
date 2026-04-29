0128da0105e6442b5cec2d7a8dc4e48ea878e5e4
Yash-Raj-5424 <myash3499@gmail.com>
2026-04-29 15:51:29 +0530

    chore: add TestController for local algorithm testing

---
+++ src/main/java/com/ratelimiter/FluxWard/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.ratelimiter.FluxWard.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "ok";
+    }
+
+    @GetMapping("/test/fixedwindow")
+    public String testLogin(){
+        return "fixed_window ok";
+    }
+
+    @GetMapping("/test/slidingwindow")
+    public String testSearch(){
+        return "sliding_window ok";
+    }
+}
