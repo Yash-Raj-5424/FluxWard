423d9ec9f2f9947d714538b0f5a628176857e2d7
Yash-Raj-5424 <myash3499@gmail.com>
2026-04-29 15:53:18 +0530

    separate class for TokenBucket

--- src/main/java/com/ratelimiter/FluxWard/core/algorithm/TokenBucketRateLimiter.java
+++ src/main/java/com/ratelimiter/FluxWard/core/algorithm/TokenBucketRateLimiter.java
@@ -4,21 +4,20 @@ import com.ratelimiter.FluxWard.core.RateLimiter;
 import com.ratelimiter.FluxWard.model.RateLimitResult;
 import com.ratelimiter.FluxWard.model.RateLimitRule;
 import com.ratelimiter.FluxWard.store.RateLimitStore;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.time.Clock;
 
 @Component
+@RequiredArgsConstructor
 public class TokenBucketRateLimiter implements RateLimiter {
 
+    private final RateLimitStore store;
     private final Clock clock;
-
-    public TokenBucketRateLimiter(RateLimitStore store, Clock clock) {
-        this.clock = clock;
-    }
 
     @Override
     public RateLimitResult tryAcquire(String clientKey, RateLimitRule rule) {
-        return null;
+        return store.getAndIncrement(clientKey, rule, clock.instant());
     }
 }
